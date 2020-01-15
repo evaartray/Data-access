9ac0985102d1092448d1c5fd6982df9c7fbe8375
pzx98720@studenci.wsb.poznan.pl <pzx98720@studenci.wsb.poznan.pl>
2020-01-15 11:55:54 +0100

    Add project files.

---
+++ DataAccess/Connection.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    class Connection : IConnection
+    {
+        private bool isOpen;
+        public IDatabase Database => throw new NotImplementedException();
+
+        public void Close()
+        {
+            if (isOpen) isOpen = false;
+            else throw new ConnectionClosedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetPassword()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetUsername()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsOpen()
+        {
+            return isOpen;
+        }
+
+        public void Open()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataAccess/Database.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    class Database : IDatabase
+    {
+        public List<Dictionary<string, object>> GetData(IConnection connection)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetDatabaseName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IConnection GetNewConnection(string userName, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
